8f4475da6c880e7b475468608a284aa770d31769
Alexis Sanchez <aasanchez@gmail.com>
2016-02-23 13:11:51 +0100

    Laravel 5.2.20

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+echo ">>> Installing SQLite Server"
+
+# Install SQL lite
+sudo apt-get install -qq sqlite
+
 cd /vagrant && composer install
+mv /vagrant/.env.example /vagrant/.env
+php artisan key:generate
