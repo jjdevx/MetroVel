16f8b07cffa94945bcfae89306f1b8efbb2ab57a
Alexis Sanchez <aasanchez@gmail.com>
2016-02-24 12:48:34 +0100

    Global Styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var elixir = require('laravel-elixir');
-
+var del = require('del');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -11,6 +11,29 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
+    return del([
+        'public/css/',
+        'public/fonts/'
+    ]);
+});
+
+elixir(function (mix) {
+    //fontawesome
+    mix.copy('node_modules/font-awesome/css/font-awesome.css', 'public/css/font-awesome.css');
+    mix.copy('node_modules/font-awesome/fonts/', 'public/fonts/');
+
+    mix.copy('node_modules/simple-line-icons-webfont/simple-line-icons.css', 'public/css/simple-line-icons.css');
+    mix.copy('node_modules/simple-line-icons-webfont/fonts/', 'public/fonts/');
+
+    mix.copy('node_modules/bootstrap/dist/css/bootstrap.css', 'public/css/bootstrap.css');
+    mix.copy('node_modules/bootstrap/dist/fonts/', 'public/fonts/');
+
+    mix.copy('node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css', 'public/css/bootstrap-switch.css');
+
+});
+
+
+elixir(function (mix) {
     mix.sass('app.scss');
 });
